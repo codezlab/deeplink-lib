f725b08ff70b9d22011de7af2b975043e3199e82
Satvinder Singh <satvinder@bijnis.com>
2024-09-09 18:49:51 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "deeplink-lib"
+include(":app")
+include(":deep-link-lib")
